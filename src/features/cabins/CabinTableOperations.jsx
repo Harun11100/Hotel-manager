8acf144788e0c6541c7ccd683b03882dd5bce1d1
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-05-20 22:38:05 +0600

    Make the filtering component reusable

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,8 +1,13 @@
 import TableOperations from "../../ui/TableOperations"
 import Filter from "../../ui/Filter"
 function CabinTableOperations() {
-      return (<TableOperations>
-        <Filter/>
+      return (
+      <TableOperations>
+        <Filter filterField='discount' 
+        options={[{value:'all',label:'All'},
+        {value:'no-discount',label:'No discount'},
+        {value:'with-discount',label:'With discount'}
+        ]}/>
       </TableOperations>
       )
 }
