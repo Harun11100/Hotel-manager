0fcaceb3d97db995d4064e799fd2ffdeb9cbdb5c
Harun11100 <145543162+Harun11100@users.noreply.github.com>
2024-05-23 20:03:43 +0600

    LogOut Button added

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -9,7 +9,7 @@ const { data, error } = await supabase.auth.signInWithPassword({
       password
     });
     if(error) throw new Error(error.message);
-    console.log(data)
+   
     
     return data
 
@@ -21,10 +21,17 @@ export async function getCurrentUser(){
      if(!session.session)return null;
 
      const {data,error}=await supabase.auth.getUser()
-    console.log(data);
+ 
     if(error) throw new Error(error.message);
     
     
     return data?.user;
    
 }
+
+export async function logout(){
+
+   const {error}=await supabase.auth.signOut()
+   if(error) throw new Error(error.message);
+    
+  }
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,38 +1,38 @@
+import styled from "styled-components"
 import { useNavigate } from "react-router-dom"
 import { useUser } from "../features/authentication/useUser"
 import Spinner from "./Spinner"
-import styled from "styled-components"
+
 import { useEffect } from "react"
 
 
 const FullPage=styled.div`
-height:100vh;
+height: 100vh;
 background-color: var(--color-grey-50);
 display: flex;
 align-items: center;
 justify-content: center;
 
-`
+`;
 
 function ProtectedRoute({children}) {
+     
 
       const navigate=useNavigate();  
 
       // 1. Load the authenticated User
-       const {isAuthenticated,isLoading}=useUser()
+       const {isAuthenticated,isLoading}=useUser();
 
 
 
       //2. If there is No Authenticated user , redirect  to the log in page
        useEffect(function(){
        if(!isAuthenticated && isLoading) navigate('/login');
-       },[isAuthenticated,isLoading,navigate])
+       },[isAuthenticated,isLoading,navigate]
+      );
        
       // 3.While Loading ,show a spinner
-      if(isLoading) return (<FullPage> 
-            <Spinner/>
-            </FullPage> 
-      )
+      if(isLoading)return(<FullPage><Spinner/></FullPage>);
 
       // 4 .If there is a user , render the app
 
